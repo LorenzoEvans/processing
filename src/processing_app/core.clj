73eb0d60ca32878c90340abf554b94242ecd9f5b
LorenzoEvans <lorenzo.evans94@gmail.com>
2019-06-22 23:21:32 -0400

    Debugging namespace issues.

--- src/processing_app/core.clj
+++ src/processing_app/core.clj
@@ -1,5 +1,24 @@
 (ns processing-app.core
   (:gen-class
-    :extends processing.core.PApplet)
-  :import )
+    :extends processing-app.core.PApplet)
+  (:import (processing.core PConstants PApplet)))
 
+
+(defn main [& args]
+  (PApplet/main "processing_with_clojure.core"))
+
+(defn -settings [this]
+  (.size this 640 480 PConstants/P3D))
+
+(defn -setup [this]
+  (.background this 255 255 255))
+
+(defn -draw [this]
+  (if (.-mousePressed this)
+    (.fill this 0)
+    (.fill this 255))
+  (let [x (.-mouseX this)
+        y (.-mouseY this)
+        width 80
+        width 80]
+    (.ellipse this x y width height)))
